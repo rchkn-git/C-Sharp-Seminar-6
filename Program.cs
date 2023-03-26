8c7ba479a6e3d20b8331cae950414678f7424aec
Aleksandr Ruchkan <aruchkan@yandex.ru>
2023-03-26 16:21:25 +0300

    Seminar 6 done

--- Program.cs
+++ Program.cs
@@ -23,7 +23,7 @@ string str1 = Convert.ToString(number, 2);
 
 Console.WriteLine(number + " в двоичном виде = " + str1);
 */
-
+/*
 // Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
 
 // Если N = 5 -> 0 1 1 2 3
@@ -64,6 +64,79 @@ switch (size)
     Console.WriteLine("Неверное значение массива.");
     break;
     default:
-    Console.WriteLine($"[{String.Join(",", FibArray)}]");
+    Console.WriteLine($"[{String.Join(", ", FibArray)}]");
     break;
+}
+*/
+
+// Задача 45: Напишите программу, которая будет создавать копию заданного массива
+// с помощью поэлементного копирования.
+
+//генерируем рандомный массив
+/*
+int[] arrayNew = GetRandomArray(6, -10, 10);
+Console.WriteLine($"[{String.Join(", ", arrayNew)}]");
+Console.WriteLine($"[{String.Join(", ", CopyMyArray(arrayNew))}]");
+*/
+
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+
+    return result;
+}
+
+
+int[] CopyMyArray(int[] array)
+{
+    int[] arrayCopy = new int[array.Length];
+    for(int i = 0; i < array.Length; i++)
+    {
+        arrayCopy[i] = array[i];
+    }
+    return arrayCopy;
+}
+
+// Задача 39: Напишите программу, которая перевернёт одномерный массив
+// (последний элемент будет на первом месте,
+// а первый - на последнем и т.д.)
+
+// [1 2 3 4 5] -> [5 4 3 2 1]
+
+// [6 7 3 6] -> [6 3 7 6]
+
+// Решить 2 способами: 1) менять числа местами в исходном массиве;
+// 2) создать новый массив и в него записать перевёрнутый исходный массив по элементам.
+
+int[] arrayA = GetRandomArray(6, -10, 10);
+Console.WriteLine($"[{String.Join(", ", arrayA)}]");
+//Console.WriteLine($"[{String.Join(", ", ArrayBackwardVar1(arrayA))}]");
+Console.WriteLine($"[{String.Join(", ", ArrayBackwardVar2(arrayA))}]");
+
+int[] ArrayBackwardVar1(int[] array)
+{
+    int len = array.Length;
+    int buff = array[0];
+    for(int i = 1; i <= (array.Length / 2); i++)
+    {
+        buff = array[len - i];
+        array[len - i] = array[i - 1];
+        array[i - 1] = buff;
+    }
+    return array;
+}
+
+int[] ArrayBackwardVar2(int[] array)
+{
+    int len = array.Length;
+    int[] ArrayBackward = new int[len];
+    for(int i = 0; i < len; i++)
+    {
+        ArrayBackward[i] = array[len - (i + 1)];
+    }
+    return ArrayBackward;
 }
